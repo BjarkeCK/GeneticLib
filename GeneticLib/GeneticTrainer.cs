80b2d06064bdf7e7bee8efe1db3b8f023bafe9f1
Bjarke Elias Clauson-Kaas Ørrild <bjarkeelias@gmail.com>
2015-04-09 22:03:25 +0200

    Fittest chromosome is guarantied to be carried over to the next generation

--- GeneticLib/GeneticTrainer.cs
+++ GeneticLib/GeneticTrainer.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using AForge.Neuro;
 
 namespace GeneticLib
 {
@@ -19,14 +20,25 @@ namespace GeneticLib
             this._population = new Population<T>(population);
             this._fitnessFunction = fitnessFunction;
             this._selection = selection;
+            this.FittestChromosome = population[0];
+            this.CurrentGeneration = 0;
         }
 
+        public Population<T> Population { get { return _population; } }
+
+        public int CurrentGeneration { get; private set; }
+
+        /// <summary>
+        /// Evolve the the population to the next generation.
+        /// </summary>
         public void Evolve()
         {
             foreach (T chromosome in _population)
             {
                 chromosome.FitnessValue = _fitnessFunction.Invoke(chromosome);
             }
+
+            GetFittestChromsome();
 
             IList<T> selection = _selection.ApplySelection(_population).Select(x => x.Clone()).ToArray();
 
@@ -35,18 +47,41 @@ namespace GeneticLib
                 function.Execute(selection, this._population);
             }
 
+            EnsureSurvival();
+
             foreach (T chromosome in _population)
             {
                 chromosome.OnEvolved();
             }
+
+            this.CurrentGeneration++;
         }
 
-        public Population<T> Population { get { return _population; } }
-
-        public T GetFittestChromosome()
+        /// <summary>
+        /// Ensures that the best chromosome gets carried over to the new generation.
+        /// </summary>
+        private void EnsureSurvival()
         {
-            return _population.OrderByDescending(x => x.FitnessValue).First();
+            for (int i = 0; i < FittestChromosome.GetNumberOfGenes(); i++)
+            {
+                _population[0].SetGene(FittestChromosome, i);
+            }
         }
+
+        private void GetFittestChromsome()
+        {
+            double bestFitnessValue = 0;
+            foreach (T chromosome in _population)
+            {
+                if (chromosome.FitnessValue > bestFitnessValue)
+                {
+                    FittestChromosome = chromosome.Clone();
+                    bestFitnessValue = chromosome.FitnessValue;
+                }
+            }
+        }
+
+        public T FittestChromosome { get; private set; }
 
         public void AddEvolveFunction<T1>(double properbility) where T1 : IEvolveFunction, new()
         {
